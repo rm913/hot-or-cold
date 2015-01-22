13d41e1083533a6573b6cff8d8537540339c2a8d
rm913 <rachelmakovoz@gmail.com>
2015-01-22 08:24:12 -0500

    g

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
 
 $(document).ready(function(){
 	
+	function random() {
+
+		Math.floor(Math.random()*100) + 1;
+		
+	};
+	console.log(random);
+	$('#guessButton').click(function(){
+		var guess = $('#userGuess').val();
+		var num = random().val();
+
+		if(guess < num){
+			console.log('were lesss than this');
+		}
+		else{
+			console.log('maybe more than that');
+		}
+
+		
+	});
+
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
