09c9a93ac18db94357470b063c976be26b825680
rm913 <rachelmakovoz@gmail.com>
2015-01-29 14:19:28 -0500

    h

--- js/app.js
+++ js/app.js
@@ -1,25 +1,57 @@
 
 $(document).ready(function(){
 	
-	function random() {
+	var num = Math.floor(Math.random()*100) + 1;
+		console.log(num);
+	
+	
+	
+	$('form').on("click", "#guessButton", function(){
 
-		Math.floor(Math.random()*100) + 1;
-		
-	};
-	console.log(random);
-	$('#guessButton').click(function(){
+		console.log('its clicked');
+	
 		var guess = $('#userGuess').val();
-		var num = random().val();
+		guess = parseInt(guess);
 
-		if(guess < num){
-			console.log('were lesss than this');
-		}
-		else{
-			console.log('maybe more than that');
-		}
+
+			if(guess <=100) {
+				$('#guessList').append("<li>"+guess+"</li>");
+				console.log('its a number');
+				
+			}
+		});
+
+///		function diff(){
+
+//			if(guess>num){
+				
+//				console.log(guess-num);
+				//}
+//			else if (num> guess){
+				
+//				console.log(num - guess);
+				
+//			}
+//			else if (num == guess){
+				
+//				console.log("You Won!");
+				
+//	}
+//		};
+		//diff();
+	//	if( i <=20){
+	//		console.log('warm');
+	//	}
+	//	else if(20<1<=50){
+	//		console.log('cold');
+	//	}
+	//	else if(i = "You Won!"){
+	//		console.log('you won');
+	//	}
 
 		
-	});
+		
+	
 
 
 	/*--- Display information modal box ---*/
