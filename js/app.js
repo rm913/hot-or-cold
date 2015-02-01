837a32c32bfef5d8d51c4f811363fbbb2053f94b
rm913 <rachelmakovoz@gmail.com>
2015-01-31 21:14:04 -0500

    h

--- js/app.js
+++ js/app.js
@@ -1,72 +1,47 @@
 
+
 $(document).ready(function(){
 	
-<<<<<<< HEAD
-	var num = Math.floor(Math.random()*100) + 1;
-		console.log(num);
-	
-	
-	
-	$('form').on("click", "#guessButton", function(){
+	function newNum(){
 
-		console.log('its clicked');
+	 return Math.floor(Math.random()*100) + 1;
+	}
+
 	
+	
+	
+	
+	$('#guessButton').click(function(){
 		var guess = $('#userGuess').val();
 		guess = parseInt(guess);
+		console.log(guess, 'its clicked');
+	
+		
+		
 
 
 			if(guess <=100) {
 				$('#guessList').append("<li>"+guess+"</li>");
 				console.log('its a number');
-				
+		
+			}
+			else{
+				alert('Please put in a # between 1 and 100');
 			}
 		});
 
-///		function diff(){
-
-//			if(guess>num){
-				
-//				console.log(guess-num);
-				//}
-//			else if (num> guess){
-				
-//				console.log(num - guess);
-				
-//			}
-//			else if (num == guess){
-				
-//				console.log("You Won!");
-				
-//	}
-//		};
-		//diff();
-	//	if( i <=20){
-	//		console.log('warm');
-	//	}
-	//	else if(20<1<=50){
-	//		console.log('cold');
-	//	}
-	//	else if(i = "You Won!"){
-	//		console.log('you won');
-	//	}
-
-		
-		
 	
 
-
-=======
->>>>>>> 209c990e47738728b5a7e870f496e9ac69125bf2
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+//  	$(".what").click(function(){
+//    	$(".overlay").fadeIn(1000);
 
-  	});
+//  	});
 
   	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+//  	$("a.close").click(function(){
+//  		$(".overlay").fadeOut(1000);
+ // 	});
 
 });
 
